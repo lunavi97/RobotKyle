ff3b282148b0dd1b919c6f083a4b6fd2009af57e
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-12 22:59:14 -0300

    Imported Robot Kyle and added to the project with logo in button

---
+++ Assets/Scripts/KyleReaction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KyleReaction : MonoBehaviour {
+
+	Animator anim;
+	string URL;
+	public AudioSource sound;
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+		URL = "linkedin.com/in/lunavi97";
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			Reaction();
+		}
+	}
+
+	public void Reaction()
+	{
+		anim.Play("Golpe");
+		sound.Play();
+	}
+
+	public void GoToURL()
+	{
+		Application.OpenURL(URL);
+	}
+
+}
